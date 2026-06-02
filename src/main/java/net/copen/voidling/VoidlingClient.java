e8210ec52117e2fae4a45bed82de547f15cb9111
copen <bogdaskinartem85@gmail.com>
2026-06-02 17:10:12 +0300

    added everything

---
+++ src/main/java/net/copen/voidling/VoidlingClient.java
@@ -0,0 +1,10 @@
+package net.copen.voidling;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VoidlingClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
